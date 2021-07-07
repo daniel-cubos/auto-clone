3f09c1724c1b09145a2657a459ee04f3307946f4
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-07-07 14:53:01 -0300

    refactor

--- shell.js
+++ shell.js
@@ -1,35 +1,33 @@
 const shell = require('shelljs');
 
 
-const repos = "git@github.com:aldusosi/desafio-modulo-03.git,git@github.com:anabeatrizgn/desafio-modulo-03.git,git@github.com:beatriztourinho/desafio-modulo-03.git"
+const repos = [""];
 
-const splitedRepos = repos.split(',');
+// Only Users Linux
+// shell.exec('git config --global user.name ');
+// shell.exec('git config --global user.email ');
+// shell.exec('git config --global user.password ');
+
+shell.cd('challanges');
 
 
-shell.cd('challange');
+for (const repo of repos) {
 
+    try {
+        const splitedNames = repo.split(':')[1].split('/');
+        const folderName = splitedNames[0];
+        const repoName = splitedNames[1].split('.git')[0];
 
-for (const repo of splitedRepos) {
-    const splitedNames = repo.split(':')[1].split('/');
-    const folderName = splitedNames[0];
-    const repoName = splitedNames[1].split('.git')[0];
-    
-    // criar as pastas para cada aluno
-    shell.mkdir(folderName);
+        shell.mkdir(folderName);
 
-    // clonar os repositórios nas pastas corretas
-    shell.cd(`${folderName}`);
-
-    if (shell.exec(`git clone ${repo}`).code === 0) {
-        shell.echo('Cloned with success!');     
-        shell.cd(repoName);
-        shell.exec('npm install');
+        shell.cd(`${folderName}`);
+        shell.exec(`git clone ${repo}`)
+    } catch (error) {
+        continue;
+    } finally {
         shell.cd('..');
-    } else {
-        shell.echo('Error: Git commit failed');
-        shell.exit(1);
     }
-    shell.cd('..');
+
 }
 
 console.log("Done")
